54f67be52fe329fa5aa50ad165f08142b2007836
Leo <ujfox13@gmail.com>
2022-03-12 00:20:32 +0300

    minor style changes in Menu list and mixins

--- src/pages/AdminMenuPage/AdminMenuPage.jsx
+++ src/pages/AdminMenuPage/AdminMenuPage.jsx
@@ -8,24 +8,49 @@ const AdminMenuPage = () => {
         <div className={styles.container}>
             <div className={styles.flex_container}>
             <AdminHeader/>
+
                 <input className={[styles.menu_input].join(' ')} type="text" placeholder='добавление продукта'></input>
                 
                 <div className={styles.menu_list}>
+
                     <div className={styles.menu_list__inner}>
                         <span>(список продуктов в меню)</span>
                         <div className={styles.btn_delete} onClick={(e) => console.log("удалить элемент", e.target)}></div>
+                        <div className={styles.btn_put} onClick={(e) => console.log("удалить элемент", e.target)}>изменить</div>
                     </div>
 
                     <div className={styles.menu_list__inner}>
                         <span>(список продуктов в меню)</span>
                         <div className={styles.btn_delete} onClick={(e) => console.log("удалить элемент", e.target)}></div>
+                        <div className={styles.btn_put} onClick={(e) => console.log("удалить элемент", e.target)}>изменить</div>
                     </div>
 
-                    <div className={styles.menu_list__inner}>(список продуктов в меню)</div>
-                    <div className={styles.menu_list__inner}>(список продуктов в меню)</div>
-                    <div className={styles.menu_list__inner}>(список продуктов в меню)</div>
-                    <div className={styles.menu_list__inner}>(список продуктов в меню)</div>
-                </div>
+                    <div className={styles.menu_list__inner}>
+                        <span>(список продуктов в меню)</span>
+                        <div className={styles.btn_delete} onClick={(e) => console.log("удалить элемент", e.target)}></div>
+                        <div className={styles.btn_put} onClick={(e) => console.log("удалить элемент", e.target)}>изменить</div>
+                    </div>
+
+                    <div className={styles.menu_list__inner}>
+                        <span>(список продуктов в меню)</span>
+                        <div className={styles.btn_delete} onClick={(e) => console.log("удалить элемент", e.target)}></div>
+                        <div className={styles.btn_put} onClick={(e) => console.log("удалить элемент", e.target)}>изменить</div>
+                    </div>
+
+                    <div className={styles.menu_list__inner}>
+                        <span>(список продуктов в меню)</span>
+                        <div className={styles.btn_delete} onClick={(e) => console.log("удалить элемент", e.target)}></div>
+                        <div className={styles.btn_put} onClick={(e) => console.log("удалить элемент", e.target)}>изменить</div>
+                    </div>
+                    
+
+                    <div className={styles.menu_list__inner}>
+                        <span>(список продуктов в меню)</span>
+                        <div className={styles.btn_delete} onClick={(e) => console.log("удалить элемент", e.target)}></div>
+                        <div className={styles.btn_put} onClick={(e) => console.log("удалить элемент", e.target)}>изменить</div>
+                    </div>
+
+                </div>{/* /.menu_list */}
 
                 <div className={styles.menu_btn_container}>
                     <button className={styles.menu_btn}>оформить</button>
