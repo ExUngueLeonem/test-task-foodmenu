89ac1901af7377b4249eade226e7ce5746d99fef
Leo <ujfox13@gmail.com>
2022-03-12 21:34:49 +0300

    Admin Menu and Item list fetching

--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import styles from "./MenuList.module.scss";
 
-const MenuList = ({ menu }) => {
+const MenuList = ({
+    menu,
+    onDeleteHandler,
+    onPutHandler
+}) => {
     return (
         <div className={styles.menu_list}>
             {menu.map((menuItem) => {
                 return (
-                    <div key={menuItem} className={styles.menu_list__inner}>
-                        <span>{menuItem}</span>
-                        <div className={styles.btn_delete} onClick={(e) => console.log("удалить элемент", e.target)}></div>
-                        <div className={styles.btn_put} onClick={(e) => console.log("изменить элемент", e.target)}>изменить</div>
+                    <div key={menuItem.id} className={styles.menu_list__inner}>
+                        <span>{menuItem.food}</span>
+                        <div className={styles.btn_delete} onClick={(e) => onDeleteHandler(menuItem.id)}></div>
+                        <div className={styles.btn_put} onClick={(e) => onPutHandler(menuItem.id)}>изменить</div>
                     </div>
                 )
             })}
--- src/pages/AdminMenuPage/AdminMenuPage.jsx
+++ src/pages/AdminMenuPage/AdminMenuPage.jsx
@@ -3,6 +3,7 @@ import styles from './AdminMenuPage.module.scss';
 import AdminHeader from '../../components/AdminHeader';
 import MenuList from '../../components/MenuList';
 import { SERVER_URL } from '../../html/const';
+import { axios } from '../../html/axios';
 
 
 const AdminMenuPage = () => {
@@ -10,17 +11,10 @@ const AdminMenuPage = () => {
     const [addFoodInput, setAddFoodInput] = useState('');
 
     const fetchMenu = () => {
-        fetch(SERVER_URL + '/menu')
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                setMenu(data)
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    };
+        axios.get(SERVER_URL + '/menu')
+            .then((res) => { setMenu(res.data) })
+            .catch(error => { console.log(error) });
+    }
 
     useEffect(
         fetchMenu, []
@@ -33,33 +27,54 @@ const AdminMenuPage = () => {
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
-        setMenu(menu.concat(addFoodInput))
+        setMenu(menu.concat({
+            id: new Date(),
+            food: addFoodInput
+        }))
         console.log(menu);
+        axios.post('http://localhost:3000/menu', {
+            id: new Date(),
+            food: addFoodInput
+        }).then(resp => {
+            console.log(resp);
+        }).catch(error => {
+            console.log(error);
+        });
         setAddFoodInput('');
     };
 
-    const onPublishHandler = (data) => {
-        console.log('publish', data)
-        fetch(SERVER_URL + '/menu', {
-            data
-        })
-        .then((response) => {
-            return response.json();
-        })
-/*         .then((data) => {
-            setMenu(data)
-        })
- */        .catch(error => {
+    const onPutHandler = (itemId) => {
+        axios.put(SERVER_URL + '/menu/' + itemId, {
+            id: itemId,
+            food: "asdfasdf4654 Давленый ксенос"
+        }).then(resp => {
+
+            console.log(resp.data);
+        }).catch(error => {
             console.log(error);
         });
+    }
 
-    };
+    const onDeleteHandler = (itemId) => {
+        axios.delete(SERVER_URL + '/menu/' + itemId)
+            .then(resp => {
+                console.log(resp.data)
+            }).catch(error => {
+                console.log(error);
+            });
+        setMenu(menu.filter((item) => {
+            if (item.id === itemId) return false
+            console.log('itemId', item.id)
+            return true
+        }
+        ))
+    }
 
     return (
         <div className={styles.container}>
             <div className={styles.flex_container}>
                 <AdminHeader active={'menu'} />
-                <form onSubmit={(e) => {onSubmitHandler(e)}}>
+                <form onSubmit={(e) => { onSubmitHandler(e) }}>
 
                     <input
                         className={[styles.menu_input].join(' ')}
@@ -70,12 +85,13 @@ const AdminMenuPage = () => {
                     ></input>
                 </form>
 
-                <MenuList menu={menu} />
+                <MenuList
+                    menu={menu}
+                    onPutHandler={onPutHandler}
+                    onDeleteHandler={onDeleteHandler}
+                />
 
                 <div className={styles.menu_btn_container}>
-                    <button 
-                    className={styles.menu_btn}
-                    onClick={() => {onPublishHandler(menu)}}>оформить</button>
                     <button className={styles.menu_btn}>история</button>
                 </div>
             </div>
