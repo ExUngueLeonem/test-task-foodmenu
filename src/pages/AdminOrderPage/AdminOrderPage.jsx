b60003d58286cc1ec559a98c31f0be5aa213f324
Leo <ujfox13@gmail.com>
2022-03-12 10:52:22 +0300

    Admin order page

---
+++ src/pages/AdminOrderPage/AdminOrderPage.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './AdminOrderPage.module.scss';
+import AdminHeader from '../../components/AdminHeader';
+
+const AdminOrderPage = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.flex_container}>
+            <AdminHeader/>
+
+                <input className={[styles.menu_input].join(' ')} type="text" placeholder='добавление продукта'></input>
+                
+                <div className={styles.menu_list}>
+
+                    <div className={styles.menu_list__inner}>
+                        <span>(список продуктов в меню)</span>
+                        <div className={styles.btn_delete} onClick={(e) => console.log("удалить элемент", e.target)}></div>
+                        <div className={styles.btn_put} onClick={(e) => console.log("удалить элемент", e.target)}>изменить</div>
+                    </div>
+
+
+                </div>{/* /.menu_list */}
+
+                <div className={styles.menu_btn_container}>
+                    <button className={styles.menu_btn}>оформить</button>
+                    <button className={styles.menu_btn}>история</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AdminOrderPage;
---
+++ src/pages/AdminOrderPage/index.js
@@ -0,0 +1,2 @@
+import AdminOrderPage from './AdminOrderPage';
+export default AdminOrderPage;
