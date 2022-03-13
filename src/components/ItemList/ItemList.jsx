3a3a6b896562dc3f0bd791af49c7307d8f08d645
Leo <ujfox13@gmail.com>
2022-03-13 18:00:25 +0300

    ItemList settings

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./ItemList.module.scss";
 
-const itemList = ({
+const ItemList = ({
     item,
     onDeleteHandler,
-    onPutHandler
+    onPutHandler, 
+    changeble = false,
+    btn_put = true,
+    btn_delete = true
 }) => {
+
+        const onPickHandler = (e) => {
+        if (!e.target.id) {
+            e.target.parentNode.getAttribute('id')
+            console.log(e.target.parentNode.getAttribute('id'));
+        } else {
+            console.log(e.target.id);
+        }
+    }
+
+    //const activeItem
+
+    
+
+
+    const [activeId, setActiveId] = useState('');
+
+
+    let listItemClassName = styles.item_list__inner;
+    
     return (
         <div className={styles.item_list}>
             {item.map((itemItem) => {
                 return (
-                    <div key={itemItem.id} className={styles.item_list__inner}>
+                    <div 
+                    className={listItemClassName}
+                    id={itemItem.id}
+                    key={itemItem.id} 
+                    onClick={(e) => { changeble && (onPickHandler(e))}}
+                    >
                         <span>{itemItem.food || itemItem.userName}</span>
-                        <div className={styles.btn_delete} onClick={(e) => onDeleteHandler(itemItem.id)}></div>
-                        <div className={styles.btn_put} onClick={(e) => onPutHandler(itemItem.id)}>изменить</div>
+                        {btn_delete && <div className={styles.btn_delete} onClick={(e) => onDeleteHandler(itemItem.id)}></div>}
+                        {btn_put && <div className={styles.btn_put} onClick={(e) => onPutHandler(itemItem.id)}>изменить</div>}
                     </div>
                 )
             })}
@@ -21,4 +49,4 @@ const itemList = ({
     );
 };
 
-export default itemList;
+export default ItemList;
